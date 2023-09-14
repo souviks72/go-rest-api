eab22eb1f4f0534705bd7da86625031a12c0e631
Souvik Mandal <souvikmandal@Souviks-MacBook-Pro.local>
2023-09-14 18:00:43 +0530

    setting up service layer

---
+++ internal/comment/comment.go
@@ -0,0 +1,64 @@
+package comment
+
+import (
+	"context"
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrFetchingComment = errors.New("failed to fetch comment by id")
+	ErrNotImplemented  = errors.New("not implemented")
+)
+
+// Comment - a representation of the comment
+// structure for our service
+type Comment struct {
+	ID     string
+	Slug   string
+	Body   string
+	Author string
+}
+
+type Store interface {
+	GetComment(context.Context, string) (Comment, error)
+}
+
+// Service - is the struct on which all our
+// logic will be built on top of
+type Service struct {
+	Store
+}
+
+// NewService - returns pointer to new service
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+// here we accept interface in param and return
+// struct from func, good architecture for testing
+
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	fmt.Println("retrieving a comment")
+	cmt, err := s.Store.GetComment(ctx, id)
+	if err != nil {
+		fmt.Println(err)
+		return Comment{}, ErrFetchingComment
+	}
+
+	return cmt, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, cmt Comment) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
+}
