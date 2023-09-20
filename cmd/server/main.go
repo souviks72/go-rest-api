6328ffacac0d52eb46e5189b470d69d239ae8ec0
Souvik Mandal <souvikmandal@Souviks-MacBook-Pro.local>
2023-09-20 18:57:50 +0530

    added db connection and migration

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/souviks72/go-rest-api/internal/db"
+)
 
 // Run - is going to be responsible for
 // the instatiation and startup of our
 // go application
 func Run() error {
 	fmt.Println("starting up our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate database")
+		return err
+	}
 	return nil
 }
 
